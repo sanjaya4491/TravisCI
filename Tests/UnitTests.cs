abe3ad33066d4f1ad6743f21608088c58cba0014
sanjaya4491 <54679398+sanjaya4491@users.noreply.github.com>
2021-03-30 01:59:34 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -33,14 +33,43 @@ namespace TravisCILab
         // Implement 3 tests per operation, following a similar pattern as above
 
 
+        // test for Multiplication.
+
+        [Test]
+        public void Multiply_Valid_Sanjaya()
+        {
+            Assert.AreEqual(4, Program.Subtract("2", "2"));
+            Assert.AreEqual(6, Program.Subtract("3", "2"));
+            Assert.AreEqual(81, Program.Subtract("9", "9"));
+        }
+
+        [Test]
+        public void Multiply_InValid_Sanjaya()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_Null_Sanjaya()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+
+
+
         // test for subtraction.
 
         [Test]
         public void Subtract_Valid_Sanjaya()
         {
-            Assert.AreEqual(3, Program.Subtract("1", "2"));
-            Assert.AreEqual(5, Program.Subtract("3", "2"));
-            Assert.AreEqual(12, Program.Subtract("5", "7"));
+            Assert.AreEqual(0, Program.Subtract("2", "2"));
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(2, Program.Subtract("9", "7"));
         }
 
         [Test]
@@ -59,7 +88,6 @@ namespace TravisCILab
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
         }
 
-        // Implement 3 tests per operation, following a similar pattern as above
 
     }
 }
