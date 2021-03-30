c06dd48915c75009ebb05e0e8ac9b81feb054a66
sanjaya4491 <54679398+sanjaya4491@users.noreply.github.com>
2021-03-30 02:05:15 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -38,25 +38,25 @@ namespace TravisCILab
         [Test]
         public void Multiply_Valid_Sanjaya()
         {
-            Assert.AreEqual(4, Program.Subtract("2", "2"));
-            Assert.AreEqual(6, Program.Subtract("3", "2"));
-            Assert.AreEqual(81, Program.Subtract("9", "9"));
+            Assert.AreEqual(4, Program.Multiply("2", "2"));
+            Assert.AreEqual(6, Program.Multiply("3", "2"));
+            Assert.AreEqual(81, Program.Multiply("9", "9"));
         }
 
         [Test]
         public void Multiply_InValid_Sanjaya()
         {
-            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
-            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
-            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
         }
 
         [Test]
         public void Multiply_Null_Sanjaya()
         {
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
         }
 
 
@@ -87,6 +87,62 @@ namespace TravisCILab
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
         }
+
+
+
+        // test for division.
+
+        [Test]
+        public void Divide_Valid_Sanjaya()
+        {
+            Assert.AreEqual(1, Program.Divide("2", "2"));
+            Assert.AreEqual(2, Program.Divide("4", "2"));
+            Assert.AreEqual(9, Program.Divide("9", "1"));
+        }
+
+        [Test]
+        public void Divide_InValid_Sanjaya()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_Null_Sanjaya()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+
+        // test for power.
+
+        [Test]
+        public void Power_Valid_Sanjaya()
+        {
+            Assert.AreEqual(4, Program.Power("2", "2"));
+            Assert.AreEqual(16, Program.Power("4", "2"));
+            Assert.AreEqual(9, Program.Power("9", "1"));
+        }
+
+        [Test]
+        public void Power_InValid_Sanjaya()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [Test]
+        public void Power_Null_Sanjaya()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
+
 
 
     }
